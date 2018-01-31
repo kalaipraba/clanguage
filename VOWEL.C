934921163a5e8b583d77494be6d0ac46be114bfe
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-01-31 17:00:06 +0530

    VOWEL.C

---
+++ VOWEL.C
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+int main()
+{
+char c
+int is lowercasevowel,is uppercasevowel;
+printf("enter an alphabet:");
+scanf("%c",&c);
+if(is lowercasevowel// is uppercasevowel)
+printf("%c is a vowel",c)
+else 
+printf("%c is a consonet",c)
+}
