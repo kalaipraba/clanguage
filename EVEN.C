fcf2f9e6d0d00aae239f9a54271c328294e3f576
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-01-31 16:48:41 +0530

    EVEN.C

---
+++ EVEN.C
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+int main()
+{
+int number;
+printf("enter an integer:");
+scanf("%d"'&number);
+if(number %2==0)
+printf("%d is a even.",number);
+else
+printf("%d is a odd.",number);
+return 0;
+}
